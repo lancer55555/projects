9dcf8c3aef27694b00296e427ad72473efccb77f
lanser55555 <black.holl@bk.ru>
2019-03-20 15:24:49 +0500

    add per

---
+++ app/spec.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  char letter = 'A';
+  double dec = 5.489;
+  int number = 100;
+  printf("%c - type char\n", letter);
+  printf("%g - type double\n", dec);
+  printf("%d - type int\n", number);
+  return 0;
+}
